ad3a84079caffe24179c180045362f1f2c55bc08
Anjali240701 <144019377+Anjali240701@users.noreply.github.com>
2024-03-04 19:25:19 +0530

    Add files via upload

--- src/component/NewExpense/ExpenseForm.js
+++ src/component/NewExpense/ExpenseForm.js
@@ -19,12 +19,25 @@ const ExpenseForm = () => {
  const locationChangeHandler = (event) => {
     setEnteredLocation(event.target.value);
  };
+
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
   };
 
+  const submitHandler = (event)=> {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+      location: enteredLocation,
+
+    };
+    console.log(expenseData);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
