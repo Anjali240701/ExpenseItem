18bfb55ca358fdaeadaf3e8023ab68dbc4fda0f5
Anjali240701 <144019377+Anjali240701@users.noreply.github.com>
2024-03-05 17:57:25 +0530

    Add files via upload

--- src/component/NewExpense/ExpenseForm.js
+++ src/component/NewExpense/ExpenseForm.js
@@ -28,7 +28,7 @@ const ExpenseForm = (props) => {
     event.preventDefault();
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: + enteredAmount,
       date: new Date(enteredDate),
       location: enteredLocation,
 
@@ -79,6 +79,7 @@ props.onSaveExpenseData(expenseData);
         </div>
       </div>
       <div className='new-expense__actions'>
+        <button type="button" onClick={props.onCancel}>Cancel</button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
